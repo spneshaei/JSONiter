b360ba115729d670abba688745a478a958fd62f0
Seyyed Parsa Neshaei <spneshaei@gmail.com>
2020-02-03 09:35:51 +0330

    Initial Commit

---
+++ JSONITER2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  JSONITER2
+//
+//  Created by Seyyed Parsa Neshaei on 2/3/20.
+//  Copyright © 2020 Seyyed Parsa Neshaei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
